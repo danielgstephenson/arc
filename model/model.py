b2967d4d5086956180039d4b658ba7e50355ee51
danielgstephenson <dgstephenson.econ@gmail.com>
2025-11-21 11:22:09 -0500

    path names

--- model/model.py
+++ model/model.py
@@ -14,6 +14,13 @@ from collections import OrderedDict
 import matplotlib.pyplot as plt
 
 os.system('clear')
+# data_path = '/teamspace/studios/this_studio/arc/model/parameters.json'
+# checkpoint_path = '/teamspace/studios/this_studio/arc/model/checkpoint.pt'
+# json_path = '/teamspace/studios/this_studio/arc/model/parameters.json'
+data_path = '../data.csv'
+checkpoint_path = 'checkpoint.pt'
+json_path = 'parameters.json'
+
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 print("device = " + str(device))
@@ -37,7 +44,7 @@ def score(state0: Tensor, state1: Tensor) -> Tensor:
 	dist1 = torch.sqrt(torch.sum(position1**2,dim=1))
 	return (dist1 - dist0).unsqueeze(1)
 
-df = pd.read_csv('../data.csv', header=None)
+df = pd.read_csv(data_path, header=None)
 
 class MyDataset(Dataset):
 	def __init__(self, df: pd.DataFrame):
@@ -111,23 +118,21 @@ old_model = ActionValueModel().to(device)
 model = ActionValueModel().to(device)
 optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
-if os.path.exists('checkpoint.pt'):
+if os.path.exists(checkpoint_path):
 	print('loading checkpoint...')
-	checkpoint = torch.load('checkpoint.pt')
+	checkpoint = torch.load(checkpoint_path)
 	model.load_state_dict(checkpoint['state_dict'])
 	old_model.load_state_dict(checkpoint['state_dict'])
 
 def save_checkpoint():
 	state_dict = model.state_dict()
-	checkpoint = { 
-		'state_dict': state_dict
-	}
-	torch.save(checkpoint,'checkpoint.pt')
+	checkpoint = { 'state_dict': state_dict }
+	torch.save(checkpoint,checkpoint_path)
 	ordered_dict = OrderedDict()
 	for key, value in state_dict.items():
 		if isinstance(value, Tensor):
 			ordered_dict[key] = value.detach().cpu().tolist()
-	with open('parameters.json','w') as file:
+	with open(json_path,'w') as file:
 	 	json.dump(ordered_dict, file, indent=4)
 
 discount = 0.1
@@ -176,7 +181,6 @@ reward = score1 - score0
 with torch.no_grad():
 	target = reward + (1-dt*discount)*old_model.value(s01,s11)
 target = target.squeeze(1)
-
 x = output.detach().cpu().numpy()
 y = target.detach().cpu().numpy()
 plt.ion()
